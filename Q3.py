a7d016d2958b85c816f77be5479d1f26a4635d62
Ajoy Sinha <ajoykumarsinha@gmail.com>
2019-03-21 18:58:30 +0530

    Add Q3

---
+++ Q3.py
@@ -0,0 +1,19 @@
+# With a given integral number n, write a program to generate a dictionary that contains
+# (i, i*i) such that is an integral number between 1 and n (both included). and then the program should print the dictionary.
+# Suppose the following input is supplied to the program:
+# 8
+# Then, the output should be:
+# {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+
+result = []
+
+def sqrroot(i):
+    return i * i
+
+
+def loopthroughno(number):
+    for i in range(number+1) :
+        result.append(str(i) +": "+str(sqrroot(i))+", ")
+    return result
+        
+print(loopthroughno(int(input())))        
